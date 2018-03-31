f6fbcb885d59c07e7e30c2c93652815e9495c0ca
evgeniy.knizhnik <megajoe1717@gmail.com>
2018-03-31 20:09:30 +0300

    Cool

--- project/food_sites/admin.py
+++ project/food_sites/admin.py
@@ -10,5 +10,20 @@ class FoodWebsiteAdmin(admin.ModelAdmin):
 
 @admin.register(FoodProduct)
 class FoodProductAdmin(admin.ModelAdmin):
-    list_display = ['food_website', 'image', 'name', 'weight', 'price', ]
+    list_display = ['food_website__name', 'image_url', 'name', 'weight', 'price', ]
+    search_fields = ['name']
     list_filter = ['food_website']
+
+    def food_website__name(self, obj):
+        return obj.food_website.name
+
+    food_website__name.short_description = 'Сайт'
+
+    def image_url(self, obj):
+        if obj.image == '/api/nofoto.png':
+            obj.image = 'https://kushat.com.ua/api/nofoto.png'
+            obj.save()
+        return '<img src="%s">' % obj.image
+
+    image_url.short_description = 'Фото'
+    image_url.allow_tags = True
