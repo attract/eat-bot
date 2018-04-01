f833c1661f5a00659e112e834154555ba6ad910a
evgeniy.knizhnik <megajoe1717@gmail.com>
2018-04-01 04:15:20 +0300

    Cool

--- project/food_sites/api/views.py
+++ project/food_sites/api/views.py
@@ -2,8 +2,10 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
 from rest_framework.mixins import ListModelMixin, UpdateModelMixin, RetrieveModelMixin, CreateModelMixin
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
+from core.bl.utils_helper import prn
 from food_sites.api.serializers import FoodWebsiteSerializer
 from ..models import FoodWebsite, FoodProduct
 
@@ -18,3 +20,8 @@ class FoodWebsiteView(ListModelMixin, RetrieveModelMixin, GenericViewSet):
 
     def get_queryset(self):
         return FoodWebsite.objects.distinct('id').prefetch_related("product_website")
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
